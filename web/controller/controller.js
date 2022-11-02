5432893d63673eb2467ec68c6d694fd641ce8600
ChinhTQ <chinhtq@saisystem.vn>
2022-11-02 13:19:01 +0700

    update web server

--- web/controller/controller.js
+++ web/controller/controller.js
@@ -5,13 +5,17 @@ function initData() {
         console.log("Remove old data");
         window.localStorage.removeItem('is_refresh');
         window.localStorage.removeItem('id-branch');
-    } else if(navigation.type == 'reload'){
+    } else if (navigation.type == 'reload') {
         console.log("Reload data page");
         if (window.localStorage['is_refresh'] == 'done') {
             console.log("Load data infor");
             getInforAdmin();
+            return;
         }
     }
+    window.addEventListener('load', function (ev) {
+        handleInitApp();
+    });
 }
 
 async function getInforAdmin() {
@@ -21,9 +25,26 @@ async function getInforAdmin() {
             'Content-Type': 'application/json',
         },
     });
-    const data = await res.json();
-    console.log(res.status,data);
-    window.dataAdmin = data['payload'];
+    if (res.status == 200) {
+        const data = await res.json();
+        if (data['code'] == 0) {
+            console.log('aaa');
+            window.dataAdmin = data['payload'];
+            handleInitApp();
+        }
+    }
+}
+
+function handleInitApp() {
+    _flutter.loader.loadEntrypoint({
+        serviceWorker: {
+            serviceWorkerVersion: serviceWorkerVersion,
+        }
+    }).then(function (engineInitializer) {
+        return engineInitializer.initializeEngine();
+    }).then(function (appRunner) {
+        return appRunner.runApp();
+    });
 }
 
 initData();
